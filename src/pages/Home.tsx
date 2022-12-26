c4f5525892a935193ceca2cc1d975b2ac8b3ace9
Rico Putra Pradana <pradanaricoputra@gmail.com>
2022-12-26 18:41:57 +0700

    +T2 chore: move LineChart to organisms

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,13 +1,13 @@
 import React from 'react'
 import BasePage from '../components/templates/BasePage'
-import LineChart from '../components/atoms/LineChart'
+import LineChart from "../components/organisms/LineChart"
 import { Box } from '@chakra-ui/react'
-import dummyInputData from '../components/atoms/LineChart/dummyInputData'
+import dummyInputData from '../components/organisms/LineChart/dummyInputData'
 
 const Home: React.FC = () => {
   return (
     <BasePage id="page-home">
-      <Box marginTop={200}>
+      <Box marginTop={100}>
         <LineChart
           title="GRAFIK PENILAIAN PELATIHAN"
           data={ dummyInputData }
