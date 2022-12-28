417abddfa017ffa351b39dcbf973277b56d1fd37
Rico Putra Pradana <pradanaricoputra@gmail.com>
2022-12-29 01:02:29 +0700

    +T2 feat: finish implementing dropdown option in Line Chart for filtering the dataset by category

--- src/components/atoms/Dropdown.tsx
+++ src/components/atoms/Dropdown.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useMemo, useState } from 'react'
 import { TDropdownItem } from '../../types/utils.type'
 import { Box } from '@chakra-ui/react';
 import DropdownItem from './DropdownItem';
@@ -10,17 +10,46 @@ interface Props {
 }
 
 const Dropdown: React.FC<Props> = ({ options, activeItem, onChange }) => {
+  const [showOptions, setShowOptions] = useState<boolean>(false);
+  
+  const inactiveOptions = useMemo(
+    () => options.filter(item => item.label != activeItem),
+    [activeItem]
+  );
+
+  const toggleOptions = () => {
+    setShowOptions(prevValue => !prevValue)
+  }
+
+  const onOptionClicked = (item: string) => {
+    onChange(item);
+    setShowOptions(false);
+  }
+
   return (
     <Box>
-      <DropdownItem label={ activeItem } onClick={() => {}} />
-
-      {options.map(item => (
+      {/* ACTIVE ITEM */}
+      <Box position="relative">
         <DropdownItem
-          key={ item.label }
-          label={ item.label }
-          onClick={() => onChange(item.label)}
+          label={ activeItem }
+          onClick={ toggleOptions }
+          isExpanded={ showOptions }
+          isActive
         />
-      ))}
+      </Box>
+
+      {/* OPTIONS */}
+      {!showOptions ? <></> : (
+        <Box position="absolute">
+          {inactiveOptions.map(item => (
+            <DropdownItem
+              key={ item.label }
+              label={ item.label }
+              onClick={() => onOptionClicked(item.label)}
+            />
+          ))}
+        </Box>
+      )}
     </Box>
   )
 }
--- src/components/atoms/DropdownItem.tsx
+++ src/components/atoms/DropdownItem.tsx
@@ -1,24 +1,37 @@
-import { Box, Text } from '@chakra-ui/react';
+import { Flex, Image, Text } from '@chakra-ui/react';
 import React from 'react'
+import arrowUpIcon from "../../assets/icons/arrow-up.svg";
+import arrowDownIcon from "../../assets/icons/arrow-down.svg";
 
 interface Props {
   label: string;
+  isActive?: boolean;
+  isExpanded?: boolean;
   onClick: () => void;
 }
 
-const DropdownItem: React.FC<Props> = ({ label, onClick }) => {
+const DropdownItem: React.FC<Props> = ({ label, isActive, isExpanded, onClick }) => {
   return (
-    <Box
+    <Flex
       minWidth="140px"
       padding="4px 12px"
       backgroundColor="background"
       border="1px solid #EAC1704D"
+      justifyContent="space-between"
+      alignItems="center"
       onClick={ onClick }
     >
       <Text fontSize="12px" fontFamily="Montserrat" fontWeight={500}>
         { label }
       </Text>
-    </Box>
+
+      {(isActive && isExpanded)
+        ? <Image src={arrowDownIcon} width="12px" />
+        : (isActive && !isExpanded)
+          ? <Image src={ arrowUpIcon } width="12px" />
+          : <></>
+      }
+    </Flex>
   )
 }
 
