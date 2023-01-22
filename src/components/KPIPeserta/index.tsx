f6574d1ac58656a2f3ec210f4149fdf0560a2755
Rico Putra Pradana <pradanaricoputra@gmail.com>
2023-01-22 23:41:39 +0700

    +T16 feature(page-sosialisasi): implement KPIPeserta component

---
+++ src/components/KPIPeserta/index.tsx
@@ -0,0 +1,137 @@
+import { Box, Flex, Text } from '@chakra-ui/react'
+import React from 'react'
+
+const KPIPeserta: React.FC = () => {
+  return (
+    <Box
+      border="1px solid #EAC170"
+      borderRadius="20px"
+      fontFamily="Montserrat"
+    >
+      {/* title & percentage */}
+      <Flex
+        direction="column"
+        alignItems="center"
+        rowGap="10px"
+        paddingY="12px"
+      >
+        <Text color="#EAC170" fontSize="12px" fontWeight={400}>
+          TOBA
+        </Text>
+        <Text fontSize="25px" fontWeight={600}>
+          100%
+        </Text>
+      </Flex>
+
+      {/* target & realisasi */}
+      <Flex width="100%">
+        <Box
+          paddingY="8px"
+          borderY="1px solid #EAC170"
+          borderRight="1px solid #EAC170"
+          textAlign="center"
+          width="50%"
+        >
+          <Text
+            fontSize="13px"
+            fontWeight={300}
+            letterSpacing="-0.05em"
+            lineHeight="16px"
+          >
+            Target
+          </Text>
+          <Text
+            fontSize="13px"
+            fontWeight={600}
+            letterSpacing="-0.05em"
+            lineHeight="16px"
+          >
+            270
+          </Text>
+        </Box>
+        <Box
+          paddingY="8px"
+          borderY="1px solid #EAC170"
+          textAlign="center"
+          width="50%"
+        >
+          <Text
+            fontSize="13px"
+            fontWeight={300}
+            letterSpacing="-0.05em"
+            lineHeight="16px"
+          >
+            Realisasi
+          </Text>
+          <Text
+            fontSize="13px"
+            fontWeight={600}
+            letterSpacing="-0.05em"
+            lineHeight="16px"
+          >
+            270
+          </Text>
+        </Box>
+      </Flex>
+      
+      {/* jenis kelamin */}
+      <Flex
+        direction="column"
+        alignItems="start"
+        padding="12px 18px 18px 18px"
+        rowGap="8px"
+      >
+        {/* laki-laki */}
+        <Flex columnGap="4px" alignItems="center">
+          <Box width="12px" height="12px" background="#00F6E8"></Box>
+          <Flex direction="column" alignItems="start">
+            <Text
+              fontSize="12px"
+              fontWeight={800}
+              lineHeight="14px"
+              letterSpacing="10%"
+            >
+              100
+            </Text>
+            <Text
+              color="#EAC170"
+              fontSize="8px"
+              fontWeight={500}
+              lineHeight="10px"
+            >
+              LAKI-LAKI
+            </Text>
+          </Flex>
+        </Flex>
+
+        {/* perempuan */}
+        <Flex columnGap="4px" alignItems="center">
+          <Box width="12px" height="12px" background="#00F6E8"></Box>
+          <Flex
+            direction="column"
+            alignItems="start"
+          >
+            <Text
+              fontSize="12px"
+              fontWeight={800}
+              lineHeight="14px"
+              letterSpacing="10%"
+            >
+              170
+            </Text>
+            <Text
+              color="#EAC170"
+              fontSize="8px"
+              fontWeight={500}
+              lineHeight="10px"
+            >
+              PEREMPUAN
+            </Text>
+          </Flex>
+        </Flex>
+      </Flex>
+    </Box>
+  )
+}
+
+export default KPIPeserta
