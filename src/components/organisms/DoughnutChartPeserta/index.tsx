0c47af8ab01ee22961d6aa96e7e7f213dfdc5b49
Rico Putra Pradana <pradanaricoputra@gmail.com>
2023-01-24 00:22:15 +0700

    +T6 feature(page-sosialisasi): pass the data required by DoughnutChartContainer in the DoughnutChartPeserta component

--- src/components/organisms/DoughnutChartPeserta/index.tsx
+++ src/components/organisms/DoughnutChartPeserta/index.tsx
@@ -1,37 +1,38 @@
 import { Box, Flex, Image, Text } from '@chakra-ui/react'
 import React from 'react'
 import ChartLine from "../../../assets/lines/chart-line.svg"
-import DougnutChart from '../DougnutChart'
 import Legends from './Legends'
 import { TGenderData } from './types/legends.types'
 import styles from "./DoughnutChartPeserta.module.css"
+import DoughnutChartContainer from '../DoughnutChart/DoughnutChartContainer'
 
-const menData: TGenderData = {
-  value: 60,
-  percent: 30,
-  label: "laki-laki",
-  color: "#00FFFF"
+interface Props {
+  title: string;
+  menData: TGenderData;
+  womenData: TGenderData;
 }
 
-const womenData: TGenderData = {
-  value: 170,
-  percent: 70,
-  label: "perempuan",
-  color: "#FF1F00"
-}
-
-const DoughnutChartPeserta: React.FC = () => {
+const DoughnutChartPeserta: React.FC<Props> = ({ title, menData, womenData }) => {
+  const chartData: TGenderData[] = [menData, womenData];
+  
   return (
     <Box>
-      <Image src={ ChartLine } width="100%" />
+      {/* CHART HEADER LINE */}
+      <Image
+        src={ ChartLine }
+        width="100%"
+      />
 
       <Flex columnGap="44px">
 
         {/* CHART */}
         <Box className={ styles.chartContainer }>
-          <DougnutChart />
+          <DoughnutChartContainer
+            data={ chartData }
+            label="Jumlah Peserta"
+          />
           <Text className={ styles.title }>
-            Peserta Sosialisasi
+            { title }
           </Text>
         </Box>
 
