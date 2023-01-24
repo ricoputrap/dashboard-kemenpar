28d012da75ecbd11acbb9b03e18d6fe6a9f0b47d
Rico Putra Pradana <pradanaricoputra@gmail.com>
2023-01-24 23:53:29 +0700

    +T6 feat(page-sosialisasi): show chart line and title on the Bar Chart

--- src/components/organisms/BarChart/index.tsx
+++ src/components/organisms/BarChart/index.tsx
@@ -9,9 +9,10 @@ import {
   Legend
 } from 'chart.js';
 import { Bar } from 'react-chartjs-2';
-import { _DeepPartialObject } from 'chart.js/dist/types/utils';
+import { Flex, Image, Text } from '@chakra-ui/react';
 import { TBarData } from './index.types';
 import useData from './useData';
+import ChartLine from "../../../assets/lines/chart-line.svg"
 
 ChartJS.register(
   CategoryScale,
@@ -23,22 +24,34 @@ ChartJS.register(
 );
 
 interface Props {
+  title: string;
   width: string;
   height: string;
   labels: string[];
   dataset: TBarData[];
 }
 
-const BarChart: React.FC<Props> = ({ width, height, labels, dataset }) => {
+const BarChart: React.FC<Props> = ({ title, width, height, labels, dataset }) => {
   const { options, data } = useData(labels, dataset);
 
   return (
-    <Bar
-      options={ options }
-      data={ data }
-      width={ width }
-      height={ height }
-    />
+    <Flex direction="column" rowGap="12px">
+      <Image src={ ChartLine } width="390px" />
+      <Text
+        fontSize="14px"
+        fontWeight={500}
+        lineHeight="16px"
+        textTransform="uppercase"
+      >
+        { title }
+      </Text>
+      <Bar
+        options={ options }
+        data={ data }
+        width={ width }
+        height={ height }
+      />
+    </Flex>
   )
 }
 
