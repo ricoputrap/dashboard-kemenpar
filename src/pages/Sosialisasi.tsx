04c584641ed31dc93439106b6569a1d205edd3d7
Rico Putra Pradana <pradanaricoputra@gmail.com>
2023-01-24 23:24:33 +0700

    +T6 chore(page-sosialisasi): implement simple bar chart

--- src/pages/Sosialisasi.tsx
+++ src/pages/Sosialisasi.tsx
@@ -1,13 +1,26 @@
-import React from 'react'
+import React, { useMemo } from 'react'
 import BasePage from '../components/templates/BasePage'
 import { Flex, Text } from '@chakra-ui/react'
 import GroupedKPIPeserta from '../components/GroupedKPIPeserta'
 import useDataSosialisasi from '../hooks/useDataSosialisasi'
 import DoughnutChartPeserta from '../components/organisms/DoughnutChartPeserta'
+import BarChart from '../components/organisms/BarChart'
+import useWindowSize from '../hooks/useWindowSize'
 
 
 const Sosialisasi: React.FC = () => {
   const { kpi, chartData, isLoading, error } = useDataSosialisasi();
+  const { width } = useWindowSize();
+
+  const barChartsWidth: string = useMemo(() => {
+    const DOUGHNUT_KPI_WIDTH = 390;
+    const PAGE_PADDING_HORIZONTAL = 56 * 2;
+    const GAP = 90;
+    const deductedWidth = DOUGHNUT_KPI_WIDTH + PAGE_PADDING_HORIZONTAL + GAP;
+    const remainingSpace = width - deductedWidth;
+    
+    return `${remainingSpace}px`;
+  }, [width]);
 
   if (chartData.length == 0 || isLoading) return (
     <BasePage id="page-sosialisasi">
@@ -17,19 +30,27 @@ const Sosialisasi: React.FC = () => {
 
   return (
     <BasePage id="page-sosialisasi">
-      <Flex direction="column" width="390px" rowGap="24px">
-        {/* Donut Chart of Jenis Kelamin Peserta */}
-        <DoughnutChartPeserta
-          title="Peserta Sosialisasi"
-          menData={ chartData[0] }
-          womenData={ chartData[1] }
-        />
+      <Flex direction="row" justifyContent="space-between">
+        {/* Doughnut Chart & KPI Peserta */}
+        <Flex direction="column" width="390px" rowGap="24px">
+          {/* Donut Chart of Jenis Kelamin Peserta */}
+          <DoughnutChartPeserta
+            title="Peserta Sosialisasi"
+            menData={ chartData[0] }
+            womenData={ chartData[1] }
+          />
 
-        {/* KPI Jumlah Peserta per Location */}
-        <GroupedKPIPeserta
-          title="Jumlah Peserta Sosialisasi DPP"
-          data={kpi}
-        />
+          {/* KPI Jumlah Peserta per Location */}
+          <GroupedKPIPeserta
+            title="Jumlah Peserta Sosialisasi DPP"
+            data={kpi}
+          />
+        </Flex>
+
+        {/* Bar Charts */}
+        <Flex direction="column" columnGap="40px" width={ barChartsWidth }>
+          <BarChart width={ barChartsWidth } height="150px" />
+        </Flex>
       </Flex>
     </BasePage>
   )
