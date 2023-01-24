04c8a6b42fbf7ff06d02bfeadfb0430515bef68d
Rico Putra Pradana <pradanaricoputra@gmail.com>
2023-01-25 00:20:09 +0700

    +T6 feat(page-sosialisasi): show bar chart for grafik usia and grafik pendidikan

--- src/pages/Sosialisasi.tsx
+++ src/pages/Sosialisasi.tsx
@@ -1,6 +1,6 @@
 import React, { useMemo } from 'react'
 import BasePage from '../components/templates/BasePage'
-import { Flex, Text } from '@chakra-ui/react'
+import { Box, Flex, Text } from '@chakra-ui/react'
 import GroupedKPIPeserta from '../components/GroupedKPIPeserta'
 import useDataSosialisasi from '../hooks/useDataSosialisasi'
 import DoughnutChartPeserta from '../components/organisms/DoughnutChartPeserta'
@@ -35,20 +35,50 @@ const datasetNilai: TBarData[] = [
   },
 ]
 
+const labelsUsia = ["<20", "21-30", "31-40", "41-50", ">50"];
+const datasetUsia: TBarData[] = [
+  {
+    label: "Jumlah",
+    data: [900, 1400, 3000, 2700, 2000],
+    backgroundColor: "#00FF38"
+  }
+]
+
+let labelsPendidikan = ["sd", "sma", "sma smk", "d1", "d2", "d3", "s1"];
+labelsPendidikan = labelsPendidikan.map(item => item.toUpperCase());
+
+const datasetPendidikan: TBarData[] = [
+  {
+    label: "Jumlah",
+    data: [1400, 2000, 2500, 3000, 2400, 2200, 1900],
+    backgroundColor: "#00FFFF"
+  }
+]
 
 const Sosialisasi: React.FC = () => {
   const { kpi, chartData, isLoading, error } = useDataSosialisasi();
   const { width } = useWindowSize();
 
-  const barChartsWidth: string = useMemo(() => {
+  const barChartsWidth: number = useMemo(() => {
     const DOUGHNUT_KPI_WIDTH = 390;
     const PAGE_PADDING_HORIZONTAL = 56 * 2;
     const GAP = 90;
     const deductedWidth = DOUGHNUT_KPI_WIDTH + PAGE_PADDING_HORIZONTAL + GAP;
     const remainingSpace = width - deductedWidth;
-    
-    return `${remainingSpace}px`;
+
+    return remainingSpace;
   }, [width]);
+
+  const barChartWidthHalf: number = useMemo(() => {
+    const PADDING = 2 * 28;
+    const GAP = 28;
+    const deductedWidth = PADDING + GAP;
+    
+    const haflWidth = Math.ceil(barChartsWidth / 2);
+    const width = haflWidth - deductedWidth - 32;
+
+    return width;
+  }, [barChartsWidth]);
 
   if (chartData.length == 0 || isLoading) return (
     <BasePage id="page-sosialisasi">
@@ -76,10 +106,10 @@ const Sosialisasi: React.FC = () => {
         </Flex>
 
         {/* Bar Charts */}
-        <Flex direction="column" rowGap="40px" width={ barChartsWidth }>
+        <Flex direction="column" rowGap="40px" width={ `${barChartsWidth}px` }>
           <BarChart
             title="Grafik Kategori Usaha"
-            width={ barChartsWidth }
+            width={ `${barChartsWidth}px` }
             height="150px"
             labels={ labels }
             dataset={ dataset }
@@ -87,11 +117,54 @@ const Sosialisasi: React.FC = () => {
 
           <BarChart
             title="Grafik Nilai Pre Test & Post Test"
-            width={ barChartsWidth }
+            width={ `${barChartsWidth}px` }
             height="150px"
             labels={ labelsNilai }
             dataset={ datasetNilai }
           />
+
+          <Flex
+            direction="column"
+            rowGap="28px"
+            padding="28px"
+            border="1px solid #EAC170"
+            borderRadius="10px"
+          >
+
+            {/* DROP DOWN */}
+            <Flex columnGap="12px" alignItems="center">
+              <Text fontSize="16px" fontWeight={500} lineHeight="20px">DPP</Text>
+              <Box
+                width="fit-content"
+                background="#EAC170"
+                borderRadius="5px"
+                padding="4px 80px 4px 16px"
+              >
+                <Text fontSize="16px" fontWeight={500} lineHeight="20px" color="#000">
+                  TOBA
+                </Text>
+              </Box>
+            </Flex>
+
+            {/* CHARTS */}
+            <Flex columnGap="24px">
+              <BarChart
+                title="Grafik Usia DPP Toba"
+                width={ `${barChartWidthHalf}px` }
+                height="150px"
+                labels={ labelsUsia }
+                dataset={ datasetUsia }
+              />
+
+              <BarChart
+                title="Grafik Pendidikan DPP TOBA"
+                width={ `${barChartWidthHalf}px` }
+                height="150px"
+                labels={ labelsPendidikan }
+                dataset={ datasetPendidikan }
+              />
+            </Flex>
+          </Flex>
         </Flex>
       </Flex>
     </BasePage>
