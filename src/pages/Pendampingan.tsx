3729435303e543eac8ced5913dfe3f49dc51c66a
Rico Putra Pradana <pradanaricoputra@gmail.com>
2023-01-29 09:42:48 +0700

    +T8 feat(page-pendampingan): make the width of Statistik boxes component responsive to the screen width

--- src/pages/Pendampingan.tsx
+++ src/pages/Pendampingan.tsx
@@ -1,15 +1,27 @@
-import React from 'react'
+import React, { useMemo } from 'react'
 import BasePage from '../components/templates/BasePage'
 import { Box, Flex } from '@chakra-ui/react';
 import useInitDataStatistik from '../components/pendampingan/hooks/useInitDataStatistik';
 import StatistikTotal from '../components/pendampingan/components/StatistikTotal';
 import Statistik from '../components/pendampingan/components/Statistik';
+import useWindowSize from '../hooks/useWindowSize';
+import { PADDING } from '../constants';
 
 const Pendampingan: React.FC = () => {
   const {
     isStatistikError,
     isStatistikLoading
   } = useInitDataStatistik();
+
+  const { width } = useWindowSize();
+
+  const statistikWidth = useMemo(() => {
+    const statistikTotalWidth = 312;
+    const totalPadding = PADDING * 2;
+    const gap = 72;
+    const result = width - statistikTotalWidth - totalPadding - gap;
+    return result;
+  }, [width]);
 
   return (
     <BasePage id="page-pendampingan">
@@ -21,7 +33,7 @@ const Pendampingan: React.FC = () => {
           </Box>
 
           {/* statistik */}
-          <Box width="1000px">
+          <Box width={`${statistikWidth}px`}>
             <Statistik />
           </Box>
         </Flex>
