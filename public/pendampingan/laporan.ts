9a26810c37b156e842a06a26aabbae1f34025646
Rico Putra Pradana <pradanaricoputra@gmail.com>
2023-02-09 13:09:56 +0700

    +T8 (pendampingan): implement data generator

---
+++ public/pendampingan/laporan.ts
@@ -0,0 +1,36 @@
+// const LOMBOK = ["Lombok Barat;Batu Layar;Pusuk Lestari",
+// "Lombok Barat;Batu Layar;Senteluk",
+// "Lombok Barat;Sekotong;Gili Gede Indah",
+// "Lombok Barat;Sekotong;Sekotong Barat",
+// "Lombok Barat;Sekotong;Buwun Mas",
+// "Lombok Tengah;Pujut;Kuta",
+// "Lombok Tengah;Pujut;Rembitan",
+// "Lombok Tengah;Praya Barat;Selong Belanak",
+// "Lombok Utara;Pemenang;Gili Indah",
+// "Lombok Utara;Tanjung;Medana",
+// "Lombok Timur;Jerowaru;Jerowaru"];
+
+// const BYP 
+// Kota;Yogyakarta	Kraton;Kadipaten
+// Kota;Kraton;Tamansari (patehan)
+// Kota;Gondomanan;Kauman (ngupasan)
+// Kota;Gedongtengen;Sosromenduran
+// Kota;Kotagede;Purbayan
+// Kota;Kraton;Warungboto
+// Kota;Kotagede;Rejowinangun
+// Kota;Gondomanan;Sayidan (prawirodirjan)
+// Kota;Kraton;Cokrodiningratan
+// Kota;Kraton;Pakualaman (gunungketur)
+// Kota;Kotagede;Prenggan
+
+// Magelang;Borobudur;Candirejo
+// Magelang;Borobudur;Tanjungsari
+// Magelang;Borobudur;Ngargogondo
+// Magelang;Borobudur;Wanurejo
+// Magelang;Mungkid;Mendut
+
+// Klaten;Prambanan;Bugisan
+// Klaten;Prambanan;Kebondalem Kidul
+// Klaten;Prambanan;Pereng
+
+// Sleman;Prambanan;Bokoharjo
