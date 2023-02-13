c41f33e6421349be9590f6e08bec3405dd4f5da9
Rico Putra Pradana <pradanaricoputra@gmail.com>
2023-02-13 22:25:42 +0700

    +T7 (pelatihan): show line & title for Materi Pelatihan

--- src/components/pelatihan/MateriPelatihan/index.tsx
+++ src/components/pelatihan/MateriPelatihan/index.tsx
@@ -1,11 +1,20 @@
 import React from 'react'
 import { Container } from './index.styles'
 import DropdownMateri from './components/DropdownMateri'
+import ChartLine from "../../../assets/lines/chart-line.svg";
+import { Flex, Image, Text } from '@chakra-ui/react';
 
 const MateriPelatihan: React.FC = () => {
   return (
     <Container>
       <DropdownMateri />
+      
+      <Flex columnGap="16px">
+        <Image src={ ChartLine } />
+        <Text fontSize="20px" fontWeight={500}>
+          MATERI PELATIHAN
+        </Text>
+      </Flex>
     </Container>
   )
 }
