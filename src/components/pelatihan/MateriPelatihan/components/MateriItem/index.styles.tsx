4adbe7978248f9b37715b3613787c7c671d25183
Rico Putra Pradana <pradanaricoputra@gmail.com>
2023-02-13 22:47:18 +0700

    +T7 (pelatihan): implement <MateriItem /> component

---
+++ src/components/pelatihan/MateriPelatihan/components/MateriItem/index.styles.tsx
@@ -0,0 +1,32 @@
+import styled from "@emotion/styled";
+
+export const Container = styled.div`
+  display: flex;
+  border: 2.29347px solid #EAC170;
+  border-radius: 5px;
+  height: 68px;
+`;
+
+export const BoxLabel = styled.div`
+  display: flex;
+  align-items: center;
+  width: 135px;
+  height: 100%;
+  padding-left: 14px;
+`;
+
+export const Label = styled.p`
+  font-size: 12px;
+  font-weight: 500;
+  text-transform: uppercase;
+  margin: 0;
+`;
+
+export const BoxIcon = styled.div`
+  width: 66px;
+  height: 100%;
+  border-left: 2.29347px solid #EAC170;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
---
+++ src/components/pelatihan/MateriPelatihan/components/MateriItem/index.tsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import { Image, LinkBox, LinkOverlay } from '@chakra-ui/react'
+import { TMateri } from '../../state/index.types'
+import { BoxIcon, BoxLabel, Container, Label } from './index.styles'
+import PdfIcon from "../../../../../assets/icons/pdf-icon.svg"
+
+const MateriItem: React.FC<TMateri> = ({ label, url }) => {
+  return (
+    <LinkBox>
+      <LinkOverlay href={ url } target="_blank">
+        <Container>
+          <BoxLabel>
+            <Label>{ label }</Label>
+          </BoxLabel>
+
+          <BoxIcon>
+            <Image
+              src={ PdfIcon }
+              width="25.5px"
+              height="30px"
+            />
+          </BoxIcon>
+        </Container>
+      </LinkOverlay>
+    </LinkBox>
+  )
+}
+
+export default MateriItem
