d7fcb2789b2e8831002bfa25d4a0f5c045e363e3
Rico Putra Pradana <pradanaricoputra@gmail.com>
2023-02-22 05:05:34 +0700

    +T12 feat(profile): implement back home button to back to the Home page

--- src/components/profile/ProfileView.tsx
+++ src/components/profile/ProfileView.tsx
@@ -1,7 +1,8 @@
 import React from 'react'
 import { useNavigate } from "react-router-dom"
 import BasePage from '../templates/BasePage'
-import { Text } from '@chakra-ui/react';
+import { Flex, Text } from '@chakra-ui/react';
+import { ArrowBackIcon } from "@chakra-ui/icons";
 
 const ProfileView: React.FC = () => {
   const navigate = useNavigate();
@@ -12,9 +13,16 @@ const ProfileView: React.FC = () => {
 
   return (
     <BasePage id="page-profile">
-      <Text onClick={ backHome }>
-        HOME
-      </Text>
+      <Flex
+        columnGap="16px"
+        alignItems="center"
+        _hover={{ cursor: "pointer" }}
+      >
+        <ArrowBackIcon height="24px" />
+        <Text onClick={ backHome }>
+          HOME
+        </Text>
+      </Flex>
     </BasePage>
   )
 }
