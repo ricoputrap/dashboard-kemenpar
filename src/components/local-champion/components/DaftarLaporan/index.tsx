3fd074cc5a53bfe816f479c00a5cbbe9155978cb
Rico Putra Pradana <pradanaricoputra@gmail.com>
2023-03-05 17:46:42 +0700

    +T18 feat(local-champion): implement computation for the width of boxes container

--- src/components/local-champion/components/DaftarLaporan/index.tsx
+++ src/components/local-champion/components/DaftarLaporan/index.tsx
@@ -1,8 +1,9 @@
-import React, { useRef } from 'react'
+import React, { useMemo, useRef } from 'react'
 import { TLaporanBulanan } from '../../state/index.types'
 import { Flex } from '@chakra-ui/react';
 import { ChevronLeftIcon, ChevronRightIcon } from '@chakra-ui/icons';
 import { BoxContainer, IconBox, Label } from './index.styles';
+import useWindowSize from '../../../../hooks/useWindowSize';
 
 interface Props {
   data: TLaporanBulanan[];
@@ -10,6 +11,31 @@ interface Props {
 
 const DaftarLaporan: React.FC<Props> = ({ data }) => {
   const boxListRef = useRef<HTMLDivElement>(null);
+  const { width } = useWindowSize();
+
+  const boxContainerWidth = useMemo(() => {
+    const PAGE_PADDING_X = 56 * 2;
+    const PAGE_CONTENT_PADDING_X = 28 * 2;
+    const PAGE_CONTENT_BORDER_X = 1 * 2;
+
+    const ROW_PADDING_LEFT = 16;
+    const AREA_WIDTH = 286;
+    const ARROWS_WIDTH = 32 * 2;
+    const GAP_WITH_ARROWS = 4 * 2;
+
+    const totalWidth = PAGE_PADDING_X
+      + PAGE_CONTENT_PADDING_X
+      + PAGE_CONTENT_BORDER_X
+      + ROW_PADDING_LEFT
+      + AREA_WIDTH
+      + ARROWS_WIDTH
+      + GAP_WITH_ARROWS
+      + 4; // extra
+
+    const containerWidth = width - totalWidth;
+    console.log("===== containerWidth:", containerWidth)
+    return containerWidth
+  }, [width])
 
   const scrollBoxes = (distance: number) => {
     if (!boxListRef.current) return;
@@ -35,7 +61,7 @@ const DaftarLaporan: React.FC<Props> = ({ data }) => {
         <ChevronLeftIcon width="32px" />
       </IconBox>
 
-      <BoxContainer width={896} ref={ boxListRef }>
+      <BoxContainer width={boxContainerWidth} ref={ boxListRef }>
         {data.map(laporan => (
           <Flex
             key={ laporan.bulan }
