d23cc5fa9fd43f856a5d89d5015ca0981ab83867
Rico Putra Pradana <pradanaricoputra@gmail.com>
2023-03-10 00:13:56 +0700

    Make the Home page responsive

--- src/components/home/DescriptionBox/index.tsx
+++ src/components/home/DescriptionBox/index.tsx
@@ -2,20 +2,25 @@ import { Box, Flex, Heading } from '@chakra-ui/react';
 import React from 'react'
 import Content from './Content';
 
-const DescriptionBox: React.FC = () => {
+interface Props {
+  height: string;
+  maxWidth?: string;
+}
+
+const DescriptionBox: React.FC<Props> = ({ height, maxWidth }) => {
   return (
     <Box
       padding="28px 8px 20px 28px"
       border="1px solid #EAC170"
       borderRadius="10px"
       minWidth="407px"
-      maxWidth="35%"
+      height={ height }
     >
       <Flex
         direction="column"
         rowGap="8px"
         overflowY="scroll"
-        height="300px"
+        height="100%"
         paddingRight="20px"
       >
         <Heading
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,10 @@
 import React from 'react'
 import BasePage from '../components/templates/BasePage'
 import DescriptionBox from '../components/home/DescriptionBox'
-import { Flex, Image } from '@chakra-ui/react'
+import { Box, Flex, Image } from '@chakra-ui/react'
 import KPI from '../components/home/organisms/KPI'
 import useDataHome from '../hooks/useDataHome'
 import MapIndo from "../assets/images/Peta.svg";
-import LoginForm from '../components/login'
 import withAuth from '../hoc/withAuth'
 
 const Home: React.FC = () => {
@@ -17,15 +16,36 @@ const Home: React.FC = () => {
   return (
     <BasePage id="page-home">
       <Flex direction="column" rowGap="32px">
-        <Flex justifyContent="space-between">
-          <DescriptionBox />
+        <Flex
+          justifyContent="space-between"
+          flexWrap="wrap-reverse"
+          gap="20px"
+        >
+          <Box
+            width={{ base: "100%", xl: "35%" }}
+            height="300px"
+          >
+            <DescriptionBox
+              height='300px'
+            />
+          </Box>
 
-          <Flex justifyContent="end">
-            <Image src={ MapIndo } width="55vw" />
+          <Flex
+            justifyContent={{ base: "center", xl: "end" }}
+            width={{ base: "100%", xl: "55vw" }}
+          >
+            <Image
+              src={ MapIndo }
+              width={{ base: "794px", xl: "55vw" }}
+            />
           </Flex>
         </Flex>
 
-        <Flex justifyContent="space-between">
+        <Flex
+          justifyContent={{ base: "center", xl: "space-between" }}
+          flexWrap={{ base: "wrap", xl: "nowrap" }}
+          gap="20px"
+        >
           <KPI data={ sosialisasi } />
           <KPI data={ pelatihan } />
           <KPI data={ pendampingan } />
