70b26840bd140fc3af37082c4b367968f7c1d754
Rico Putra Pradana <pradanaricoputra@gmail.com>
2023-03-18 23:21:18 +0700

    Implement popover statistic component for Toba area

--- src/components/home/MapIndo/ItemArea/index.tsx
+++ src/components/home/MapIndo/ItemArea/index.tsx
@@ -1,4 +1,4 @@
-import { Flex, Text } from '@chakra-ui/react'
+import { Divider, Flex, Popover, PopoverContent, PopoverTrigger, Portal, Text } from '@chakra-ui/react'
 import React from 'react'
 
 interface Props {
@@ -7,20 +7,104 @@ interface Props {
 
 const ItemArea: React.FC<Props> = ({ label }) => {
   return (
-    <Flex
-      justifyContent="center"
-      alignItems="center"
-      height="100%"
-      width="100%"
-    >
-      <Text
-        // fontSize="12px"
-        fontWeight={500}
-        color="black"
-      >
-        { label }
-      </Text>
-    </Flex>
+    <Popover placement="right-start">
+      <PopoverTrigger>
+        <Flex
+          justifyContent="center"
+          alignItems="center"
+          height="100%"
+          width="100%"
+        >
+          <Text
+            fontWeight={500}
+            color="#112647"
+          >
+            { label }
+          </Text>
+        </Flex>
+      </PopoverTrigger>
+      <Portal>
+        <PopoverContent
+          width="154px"
+          textAlign="center"
+          padding="10px 12px"
+          color="#112647"
+          background="#E9C270"
+
+          display="flex"
+
+        >
+          <Text
+            fontSize="17px"
+            fontWeight={700}
+            fontFamily="Montserrat"
+          >
+            { label }
+          </Text>
+
+          <Divider borderColor="#112647" />
+
+          <Flex direction="column" rowGap="0px">
+            <Text
+              fontSize="14px"
+              fontWeight={700}
+              fontFamily="Montserrat"
+              lineHeight="17px"
+            >
+              2022
+            </Text>
+            <Text
+              fontSize="14px"
+              fontWeight={500}
+              fontFamily="Montserrat"
+              lineHeight="17px"
+            >
+              18 Desa Wisata
+            </Text>
+          </Flex>
+
+          <Flex direction="column" rowGap="0px">
+            <Text
+              fontSize="14px"
+              fontWeight={700}
+              fontFamily="Montserrat"
+              lineHeight="17px"
+            >
+              2023
+            </Text>
+            <Text
+              fontSize="14px"
+              fontWeight={500}
+              fontFamily="Montserrat"
+              lineHeight="17px"
+            >
+              10 Desa Wisata
+            </Text>
+          </Flex>
+
+          <Divider borderColor="#112647" />
+
+          <Flex direction="column" rowGap="0px">
+            <Text
+              fontSize="14px"
+              fontWeight={700}
+              fontFamily="Montserrat"
+              lineHeight="17px"
+            >
+              Total
+            </Text>
+            <Text
+              fontSize="14px"
+              fontWeight={500}
+              fontFamily="Montserrat"
+              lineHeight="17px"
+            >
+              28 Desa Wisata
+            </Text>
+          </Flex>
+        </PopoverContent>
+      </Portal>
+    </Popover>
   )
 }
 
