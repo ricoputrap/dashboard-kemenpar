793e28ac7f7a26a7aaf036b317c5e633285b0401
Rico Putra Pradana <pradanaricoputra@gmail.com>
2023-04-12 20:35:59 +0700

    Sosialisasi: retrieve data nilai 2023 from API

--- src/API/sosialisasi/nilai.ts
+++ src/API/sosialisasi/nilai.ts
@@ -1,5 +1,6 @@
 import useSWR from "swr";
 import { fetcher } from "..";
+import { getCookie } from "../../utils/cookie";
 
 type TNilaiPerlokasi = {
   lokasi: string;
@@ -17,9 +18,23 @@ export type TNilaiResponse = {
 }
 
 const nilai = (tahun: number) => {
-  const source = `data/sosialisasi/nilai/${tahun}.json`;
-  const { data, error, isLoading } = useSWR<TNilaiResponse>(source, fetcher);
-  return { data, error, isLoading }
+  if (tahun == 2022) {
+    const source = `data/sosialisasi/nilai/${tahun}.json`;
+    const { data, error, isLoading } = useSWR<TNilaiResponse>(source, fetcher);
+    return { data, error, isLoading }
+  }
+  else if (tahun == 2023) {
+    const URL = "https://kampanye-sadarwisata.com/api/sosialisasi/nilai?tahun=2023";
+    const token = getCookie("token");
+
+    const { data, error, isLoading } = useSWR<TNilaiResponse>(
+      [URL, token],
+      ([URL, token]) => fetcher(URL, token)
+    );
+
+    return { data, error, isLoading }
+  }
+  return { data: null, error: null, isLoading: true };
 }
 
 export default nilai;
