6abf98a60b807328acb3c02e4d4a830196d41f46
Rico Putra Pradana <pradanaricoputra@gmail.com>
2023-05-09 06:46:30 +0700

    +T20 feat(publikasi): deactivate auth for Publikasi

--- src/pages/Publikasi.tsx
+++ src/pages/Publikasi.tsx
@@ -2,4 +2,5 @@ import React from 'react'
 import withAuth from '../hoc/withAuth'
 import PublikasiView from '../components/publikasi/PublikasiView'
 
-export default withAuth(PublikasiView)
+// export default withAuth(PublikasiView)
+export default PublikasiView;
